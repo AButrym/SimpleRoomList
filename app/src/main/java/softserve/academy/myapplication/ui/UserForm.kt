590aed111982274c4e18f83d6d3445ca1eff21e5
obutr <butrym@karazin.ua>
2025-10-27 19:38:16 +0200

    Introduce state management and validation for UserForm with ViewModel, improve landscape support, and refactor UI for enhanced modularity.

--- app/src/main/java/softserve/academy/myapplication/ui/UserForm.kt
+++ app/src/main/java/softserve/academy/myapplication/ui/UserForm.kt
@@ -1,18 +1,26 @@
 package softserve.academy.myapplication.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import softserve.academy.myapplication.UserField
@@ -82,7 +90,7 @@ fun UserInfoFields(uiState: UserState, userViewModel: UserViewModel) {
     ValidatedOutlinedTextField(
         value = uiState.phoneNumber,
         onValueChange = { userViewModel.onFieldChange(it, UserField.PHONE_NUMBER) },
-        label = "PhoneNumber",
+        label = "Phone Number",
         errorMessage = uiState.phoneNumberError
     )
     // TODO: add rest of the fields
